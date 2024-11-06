883130c3e2e65945dc88f012cba8aee3dad9f746
Ryan Kim <ryankim@Ryans-MacBook-Pro.local>
2024-11-06 14:35:01 -0500

    testing again

--- Assets/Scripts/MapGeneration/CityMap.cs
+++ Assets/Scripts/MapGeneration/CityMap.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CityMap : NoiseMap
 {
@@ -11,6 +13,23 @@ public class CityMap : NoiseMap
     [SerializeField] private CA_Neighborhood m_neighborhoodType = CA_Neighborhood.Moore;
     [SerializeField] private int m_neighborhoodThreshold = 5;
     [SerializeField] private int m_numIterations = 2;
+
+    private static Vector2Int[] m_mooreIndices = new Vector2Int[8] {
+        new Vector2Int(-1,-1),
+        new Vector2Int(0,-1),
+        new Vector2Int(1,-1),
+        new Vector2Int(-1,0),
+        new Vector2Int(1,0),
+        new Vector2Int(-1,1),
+        new Vector2Int(0,1),
+        new Vector2Int(1,1)
+    };
+    private static Vector2Int[] m_neumannIndices = new Vector2Int[4] {
+        new Vector2Int(0,-1),
+        new Vector2Int(-1,0),
+        new Vector2Int(1,0),
+        new Vector2Int(0,1)
+    };
 
     public override void GenerateMap() {
         // Step 0: Generate the prng system
@@ -23,7 +42,13 @@ public class CityMap : NoiseMap
         int[,] regionMap = GenerateRegions(m_noiseMap, m_terrainTypes);
 
         // Step 3: We use cellular automata processing to generate regions for real - the original regionMap is a random distribution
+        int[,] combinedMap = regionMap;
+        for(int i = 0; i < m_numIterations; i++) {
+            combinedMap = CombineRegions(combinedMap, m_neighborhoodType, m_neighborhoodThreshold);
+        }
 
+        // Finally, need to convert from combinedMap back to noiseMap
+        m_noiseMap = ConvertCombinedToTerrain(combinedMap);
 
         // Step Final: Render
         m_heightMap = Generators.GenerateHeightMap(m_noiseMap, m_textureHeightCurve, m_textureHeightMultiplier);
@@ -48,14 +73,35 @@ public class CityMap : NoiseMap
         return regionMap;
     }
 
-    /*
-    public static void MooreNeighborIndices(int x, int y, int maxX, int maxY) {
-        List<Vector2Int> indices = new List<Vector2Int>();
-        for(int yi = y-1; yi <= y+1; yi++) {
-            for(int xi = x-1; xi <= x+1; xi++) {
-                if (xi < )
+    private int[,] CombineRegions(int[,] data, CA_Neighborhood neighborhoodType, int threshold) {
+        int width = data.GetLength(0);
+        int height = data.GetLength(1);
+        int[,] combined = new int[width, height];
+        Vector2Int[] neighborIndices = neighborhoodType == CA_Neighborhood.Moore ? m_mooreIndices : m_neumannIndices;
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                int cityCount = 0;
+                foreach(Vector2Int ni in neighborIndices) {
+                    int nx = x + ni.x;
+                    int ny = y + ni.y;
+                    if (nx < 0 || nx >= width || ny < 0 || ny >= height) continue;
+                    if (data[nx,ny] == 1) cityCount += 1;
+                }
+                combined[x,y] = (cityCount >= threshold) ? 1 : 0;
             }
         }
+        return combined;
     }
-    */
+
+    private float[,] ConvertCombinedToTerrain(int[,] data) {
+        int width = data.GetLength(0);
+        int height = data.GetLength(1);
+        float[,] converted = new float[width, height];
+        for(int y = 0; y < height; y++) {
+            for(int x = 0; x < width; x++) {
+                converted[x,y] = m_terrainTypes[data[x,y]].height;
+            }
+        }
+        return converted;
+    }
 }
