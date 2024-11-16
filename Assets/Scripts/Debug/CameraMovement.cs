b683086a5e35b423f2a200879de0627a1503ddcb
henry <henry.j.kam@gmail.com>
2024-11-16 17:39:33 -0500

    add debug scene for landmark visibility

---
+++ Assets/Scripts/Debug/CameraMovement.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 5f; 
+    public HashSet<GameObject> visibleObjects = new HashSet<GameObject>();
+
+    void Update()
+    {
+        float moveX = 0f;
+        float moveZ = 0f;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            moveZ = 1f;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            moveZ = -1f;
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            moveX = -1f;
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            moveX = 1f;
+        }
+
+        Vector3 movement = new Vector3(moveX, 0f, moveZ).normalized * moveSpeed * Time.deltaTime;
+
+        transform.position += movement;
+
+        foreach (GameObject obj in visibleObjects)
+        {
+            Debug.Log($"Visible object: {obj.name}");
+        }
+    }
+
+}
